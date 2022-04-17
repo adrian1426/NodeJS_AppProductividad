94034c9f002f7f08e6a6dec0c26d9b565db1dfc7
adrian1426 <adrianhh1426@gmail.com>
2022-04-17 17:56:32 -0500

    tareaModel

---
+++ src/models/tareaModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const tarea ={
+    titulo:{
+        type: String,
+        required: true
+    },
+    descripcion:{
+        type: String,
+        required: true
+    },
+    estatus:{
+        id:{
+            type: Number,
+            required:true
+        },
+        descripcion:{
+            type: String,
+            required: true
+        }
+    },
+    tiempo:{
+        programado:{
+            type: Number,
+            required: true
+        },
+        transcurrido:{
+            type: Number,
+            required: true
+        },
+        actual:{
+            type: Number,
+            required: true
+        }
+    },
+    usuario:{
+        type: Schema.Types.ObjectId, 
+        ref: 'Usuario'
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now
+    },
+    updatedAt: { 
+        type: Date, 
+        default: Date.now
+    }
+};
+
+const tareaSchema = mongoose.Schema(tarea);
+module.exports = mongoose.model('Tarea', tareaSchema);
