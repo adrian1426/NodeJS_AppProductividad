3fa02355c650b69f9200794e2bd923d69b1bb48e
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-04-25 23:54:57 -0500

    filtros por estatus y tiempo de tareas

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,35 @@
 const port = 3001;
 
-module.exports = { port };
+const limitDefault = 10;
+const skipDefault = 0;
+
+const idStatusAll = 0;
+
+
+const tiempo1 = {
+  id: 1,
+  rangeStart: 0,
+  ramgeFinal: 1801
+};
+
+const tiempo2 = {
+  id: 2,
+  rangeStart: 1800,
+  ramgeFinal: 3601
+};
+
+const tiempo3 = {
+  id: 3,
+  rangeStart: 3600,
+  ramgeFinal: 7201
+};
+
+module.exports = {
+  port,
+  limitDefault,
+  skipDefault,
+  idStatusAll,
+  tiempo1,
+  tiempo2,
+  tiempo3
+};
--- src/services/tareaService.js
+++ src/services/tareaService.js
@@ -1,34 +1,47 @@
+const { skipDefault, limitDefault, idStatusAll, idTiempo1, idTiempo2, idTiempo3, tiempo1, tiempo2, tiempo3 } = require('../constants');
 const tareaModel = require('../models/tareaModel');
 
-const addTarea=(req,res)=>{
-    const tarea= tareaModel(req.body);
+const addTarea = (req, res) => {
+    const tarea = tareaModel(req.body);
 
     tarea.save()
-    .then(() => res.status(201).json(tarea))
-    .catch(err => res.status(400).json({ error: err }));
+        .then(() => res.status(201).json(tarea))
+        .catch(err => res.status(400).json({ error: err }));
 };
 
-const getTareaByUserId=(req,res)=>{
-    const {idUsuario}=req.params;
-    const {titulo,idStatus,idTiempo,limit,skip}=req.query;
+const getTareaByUserId = (req, res) => {
+    const { idUsuario } = req.params;
+    const { titulo, idStatus, idTiempo, limit, skip } = req.query;
 
-    const queryLimit = Number(limit) || 10;
-    const querySkip = Number(skip) || 0;
-    
+    const queryLimit = Number(limit) || limitDefault;
+    const querySkip = Number(skip) || skipDefault;
+
+    const filEstatusId = Number(idStatus) !== idStatusAll && { "estatus.id": idStatus };
+    const filTiempoProg = Number(idTiempo) === tiempo1.id ? { "tiempo.programado": { $gt: tiempo1.rangeStart, $lt: tiempo1.ramgeFinal } }
+        : Number(idTiempo) === tiempo2.id ? { "tiempo.programado": { $gt: tiempo2.rangeStart, $lt: tiempo2.ramgeFinal } }
+            : Number(idTiempo) === tiempo3.id ? { "tiempo.programado": { $gt: tiempo3.rangeStart, $lt: tiempo3.ramgeFinal } }
+                : {};
+
+    const objFilter = {
+        ...filEstatusId,
+        ...filTiempoProg
+    };
+
     tareaModel.find({
-        usuario:idUsuario,
-        $or:[
+        usuario: idUsuario,
+        ...objFilter,
+        $or: [
             { titulo: new RegExp(titulo, 'i') }
         ]
     })
-    .populate('usuario',{nombre:1})
-    .skip(querySkip)
-    .limit(queryLimit)
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(400).json({ error: err }));
+        .populate('usuario', { nombre: 1 })
+        .skip(querySkip)
+        .limit(queryLimit)
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(400).json({ error: err }));
 };
 
-module.exports={
+module.exports = {
     addTarea,
     getTareaByUserId
 };
